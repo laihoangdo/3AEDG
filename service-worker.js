ec4539fc31d018e5fcc491c5f06980db0a2e60d4
laihoangdo <laihoangdo0506@gmail.com>
2019-07-16 14:07:00 +0700

    deployed

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4c3e192196b4f0aa076aa723db3adeef.js"
+  "./precache-manifest.26eeaf0a54fdbcda55609f80065421ef.js"
 );
 
 workbox.clientsClaim();
