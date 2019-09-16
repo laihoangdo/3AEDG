9eb8e7e6e017f247da6a2588c37bacb207e46900
laihoangdo <laihoangdo0506@gmail.com>
2019-09-16 19:20:15 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b3a54ed8271c4bbda3b0dbee52564cc3.js"
+  "./precache-manifest.a791f83cf5510d0d93795f86363cb888.js"
 );
 
 workbox.clientsClaim();
--- static/js/11.e7ad3468.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{260:function(e,n,t){"use strict";t.r(n),t.d(n,"default",function(){return p});var u=t(8),a=t(9),r=t(11),c=t(10),o=t(12),i=t(0),l=t.n(i),p=function(e){function n(){return Object(u.a)(this,n),Object(r.a)(this,Object(c.a)(n).apply(this,arguments))}return Object(o.a)(n,e),Object(a.a)(n,[{key:"render",value:function(){return l.a.createElement("div",null,l.a.createElement("h1",null,"404 Page Not Found"))}}]),n}(i.PureComponent)}}]);
-//# sourceMappingURL=11.e7ad3468.chunk.js.map
